60147f0b7c8c266c53a2bae4d2536f82d3fd1bf0
bga <bga.email@gmail.com>
2021-09-08 20:26:05 +0300

    remove `swap`. Use `std::swap` instead

--- include/!cpp/common.h
+++ include/!cpp/common.h
@@ -151,10 +151,6 @@ BGA__MAYBE_CONSTEXPR Size arraySize( Type (&)[n] ) {
   return n;
 }
 
-template<typename T> void swap(T &a, T &b) {
-  T temp = a; a = b; b = temp;
-}
-
 #ifndef nop
   inline void nop() {
 
